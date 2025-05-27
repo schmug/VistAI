573375a6e055a6971967b2a41e640a00165efff7
schmug <38227427+schmug@users.noreply.github.com>
2025-05-27 18:25:03 -0400

    Add custom search location hook

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Router } from "wouter";
+import { useLocationSearch } from "./hooks/use-full-location";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -10,7 +11,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Router>
+      <Router searchHook={useLocationSearch}>
         <App />
       </Router>
       <Toaster />
